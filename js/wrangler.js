f00c6db17dbcca9d06334cc1c75bfdbb757834a7
Adam Burdett <burdettadam@gmail.com>
2016-08-22 16:07:20 -0600

    Revert "Revert "use my bootstrap""
    This reverts commit c1a32cab96e9dfbcb38fd84bac89c765f47cdd85.

--- js/wrangler.js
+++ js/wrangler.js
@@ -61,8 +61,8 @@
             "rulesets": {"prod": "b507901x1.prod",//"v1_wrangler.prod", 
                          "dev" : "b507901x1.prod" //"v1_wrangler.dev"
 			},
-            "bootstrap":{"prod": "b507199x1.prod", 
-                         "dev": "b507199x1.dev"
+            "bootstrap":{"prod": "b507901x0.prod", 
+                         "dev": "b507901x0.dev"
 			}
         };
 
